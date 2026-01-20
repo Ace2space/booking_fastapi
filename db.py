a2ac7787549ebde8f38150aae680207d1e0a494f
Aman Goyal <amangoyal@aman-goyal-THV-ITASSET-840.local>
2026-01-20 16:52:05 +0530

    Use DATABASE_URL from environment

--- db.py
+++ db.py
@@ -1,9 +1,16 @@
+# import psycopg2
+#
+# def get_connection():
+#     return psycopg2.connect(
+#         host="localhost",
+#         database="test2",
+#         user="amangoyal",
+#         password=""
+#     )
+
+
+import os
 import psycopg2
 
 def get_connection():
-    return psycopg2.connect(
-        host="localhost",
-        database="test2",
-        user="amangoyal",
-        password=""
-    )
+    return psycopg2.connect(os.environ["postgresql://aman:NtAQPvjx6v5Rwoo0o16271UQdbHUrPzN@dpg-d5nm9156ubrc73av76eg-a.singapore-postgres.render.com/booking_db_9g1d"])
